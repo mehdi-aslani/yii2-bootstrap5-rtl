f8d3466166d76ec6eceb3717e93d44e6288636e4
mehdi-aslani <aslani.mhd@gmail.com>
2022-12-21 08:04:00 +0330

    The Twitter Bootstrap 5 Rtl extension for the Yii 2.0 framework

---
+++ BootstrapRtlAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace airani\bootstrap;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for the Twitter bootstrap Rtl css files.
+ *
+ * @author Ali Irani <aslani.mhd@gmail.com>
+ * @since 2.0
+ */
+class Bootstrap5RtlAsset extends AssetBundle
+{
+    /**
+     * @inheritDoc
+     */
+    public $sourcePath = '@bower/bootstrap';
+
+    /**
+     * @inheritDoc
+     */
+    public $css = [
+        'dist/css/bootstrap.rtl.css'
+    ];
+}
